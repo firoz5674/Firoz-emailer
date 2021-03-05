c6f8b6d2f10ec00cbd1d8de64fa020af0b5cf9e5
Firoz Ahmed <firozsaifi1234@gmail.com>
2021-03-05 22:51:35 +0530

    add calculator design and functionality

---
+++ script.js
@@ -0,0 +1,65 @@
+
+
+function seven() {
+    form.result.value += '7';
+}
+function eight() {
+    form.result.value += '8';
+}
+function nine() {
+    form.result.value += '9';
+}
+function multiply() {
+    form.result.value += '*';
+}
+function four() {
+    form.result.value += '4';
+}
+function five() {
+    form.result.value += '5';
+}
+function six() {
+    form.result.value += '6';
+}
+function minus() {
+    form.result.value += '-';
+}
+function one() {
+    form.result.value += '1';
+}
+function two() {
+    form.result.value += '2';
+}
+function three() {
+    form.result.value += '3';
+}
+function add() {
+    form.result.value += '+';
+}
+// function clear() {
+//     document.getElementById('result').value = '';
+// }
+function zero() {
+    form.result.value += '0';
+}
+function dot() {
+    form.result.value += '.';
+}
+function modulas() {
+    form.result.value += '%';
+}
+function divide() {
+    form.result.value += '/';
+}
+function square() {
+    form.result.value += '**';
+}
+function equalsTo() {
+    form.result.value = eval(form.result.value);
+}
+
+
+
+
+
+
